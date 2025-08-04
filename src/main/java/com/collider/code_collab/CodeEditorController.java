ef00222cc9f90f57383ab97466e37771649f88af
Prithivi36 <prithivipappian@gmail.com>
2025-08-04 20:27:22 +0530

    Delete from room update

--- src/main/java/com/collider/code_collab/CodeEditorController.java
+++ src/main/java/com/collider/code_collab/CodeEditorController.java
@@ -18,13 +18,14 @@ import java.util.Map;
 public class CodeEditorController {
 
     SimpMessagingTemplate messagingTemplate;
+    NotepadController notepadController;
     private final Map<String,String > map = new HashMap<>();
     private final Map<String, List<String>> users = new HashMap<>();
 
-    @MessageMapping("/room/{roomId}/edit")
-    @SendTo("/topic/room/{roomId}")
-    public CodeEditor codeSocket(@DestinationVariable String roomId, CodeEditor codeEditor) {
-        map.put(roomId, codeEditor.getContent());
+    @MessageMapping("/room/{userId}/edit")
+    @SendTo("/topic/room/{userId}")
+    public CodeEditor codeSocket(@DestinationVariable String userId, CodeEditor codeEditor) {
+        map.put(userId, codeEditor.getContent());
         return codeEditor;
     }
 
@@ -37,19 +38,21 @@ public class CodeEditorController {
         users.put(roomId,user);
         System.out.println(users.get(roomId));
         messagingTemplate.convertAndSend("/topic/room/users/" + roomId, users.get(roomId));
-        System.out.println("brodcasting...");
-//        messagingTemplate.convertAndSend("/topic/room/users/" + roomId, users.get(roomId));
         return users.getOrDefault(roomId,new ArrayList<>());
     }
     @MessageMapping("room/{roomId}/del/{userId}")
     public void delete(@DestinationVariable String roomId, @DestinationVariable String userId) {
+        System.out.println("deletd");
         users.get(roomId).remove(userId);
         if(users.get(roomId).isEmpty()){
             users.remove(roomId);
+            notepadController.map.remove(roomId);
         }
         map.remove(userId);
-        if(users.containsKey(roomId))
+        if(users.containsKey(roomId)){
             messagingTemplate.convertAndSend("/topic/room/users/"+roomId, users.get(roomId));
+            messagingTemplate.convertAndSend("/topic/room/"+userId,new CodeEditor());
+        }
     }
     @MessageMapping("room/{roomId}/sync")
     public void sync(@DestinationVariable String roomId) {
--- src/main/java/com/collider/code_collab/NotepadController.java
+++ src/main/java/com/collider/code_collab/NotepadController.java
@@ -14,16 +14,18 @@ import java.util.HashMap;
 public class NotepadController {
 
     SimpMessagingTemplate messagingTemplate;
-    private final HashMap<String,String> map = new HashMap<>();
+    final HashMap<String,String> map = new HashMap<>();
     @MessageMapping("/room/{roomId}/notepad")
     @SendTo("/topic/room/{roomId}/notepad")
-    public String notepadSocket(@DestinationVariable String roomId, Notepad notepad) {
+    public Notepad notepadSocket(@DestinationVariable String roomId, Notepad notepad) {
         map.put(roomId, notepad.getContent());
-        return notepad.getContent();
+        return notepad;
     }
     @MessageMapping("room/{roomId}/sync/notepad")
     public void sync(@DestinationVariable String roomId) {
         String current = map.getOrDefault(roomId,"");
-        messagingTemplate.convertAndSend("/topic/room/" + roomId+"/notepad", current);
+        Notepad notepad = new Notepad();
+        notepad.setContent(current);
+        messagingTemplate.convertAndSend("/topic/room/" + roomId+"/notepad",notepad );
     }
 }
